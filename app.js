8318668bd0a3ff23ad0d6b38aa613f1262305a5f
tmore <tmore@kth.se>
2018-02-01 18:24:48 +0100

    Shtuff

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 var express = require('express')
+var Handlebars = require('handlebars');
+var fs = require('fs')
 var app = express()
 
+var index_source   = ""
+var index_template;
+fs.open('./index.html', 'r', function(err, fileToRead){
+    if (!err){
+	fs.readFile(fileToRead, {encoding: 'utf-8'}, function(err,data){
+	    if (!err){
+		index_source = data;
+		index_template = Handlebars.compile(index_source);
+	    }else{
+		console.log("Count't read index ");
+	    }
+	});
+    }else{
+	console.log(err);
+    }
+});
+
+var index_template = Handlebars.compile(source);
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    output = Handlebar.
+	res.send(index_template({body = "Potatis"}))
+
 })
 
 app.listen(5000)
